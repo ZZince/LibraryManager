eaca5185b015e34d58e2a94603e65e3933c11b12
Artu Florient <artuflorient0@gmail.com>
2024-12-16 11:01:06 +0100

    Etape 2: Commencer son programme

---
+++ LibraryManager.app/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager.app
+{
+    public class Book
+    {
+        private string _Name { get; set; }
+        private string _Type { get; set; }
+
+        public Book(string name, string type)
+        {
+            _Name = name;
+            _Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"Book: {_Name}, Type: {_Type}";
+        }
+    }
+
+}
--- LibraryManager.app/Program.cs
+++ LibraryManager.app/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LibraryManager.app;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        List<Book> books = new List<Book>();
+        books.Add(new Book("Le Hobbit", "Fantaisie"));
+        books.Add(new Book("L'île au trésor", "Aventure"));
+        books.Add(new Book("Le tour du monde en 80 jours", "Aventure"));
+        foreach (Book book in books)
+        {
+            Console.WriteLine(book);
+        }
+    }
+}
